58a0e15d2ae975acb68ce67c1c941261d3e7bc41
aloo <aloocs@gmail.com>
2018-01-10 16:59:48 +0800

    resovle the test case

--- public/tests/SpecHelper.js
+++ public/tests/SpecHelper.js
@@ -1,3 +1,11 @@
+/***
+ * Excerpted from "Serverless Single Page Apps",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material,
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose.
+ * Visit http://www.pragmaticprogrammer.com/titles/brapps for more book information.
+***/
 var fixture;
 
 function loadFixture(path) {  
@@ -14,7 +22,7 @@ function loadFixture(path) {
 
 function resetFixture() {
   if (!fixture) {
-    var index = $('<div>').append(loadFixture('../index.html'));
+    var index = $('<div>').append(loadFixture('/index.html'));
     var markup = index.find('div.markup');
     fixture = $('<div class="fixture" style="display: none">').append(markup);
     $('body').append(fixture.clone());
